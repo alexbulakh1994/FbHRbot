ed30a521cb42b41b14b081c1f34f19ac3c6dda57
alexbulakh1994 <alexbulakh707@gmail.com>
2016-05-05 15:07:09 +0300

    WebHook

--- index.js
+++ index.js
@@ -22,9 +22,10 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-app.get('/webhook/', function (req, res) {
+app.get('/webhook', function (req, res) {
   if (req.query['hub.verify_token'] === 'EAAD6bH65BkQBAGPizWuBi0RANKD4iZBuz09OIg83ZBzGewfLCi7BONMvU7x0637ZB7kYNlGoTTdvXzfdQPhSW03ZAdmiZAgwNSt89ZAZBeZBKWYP5TfWiZBMvVLVLlLElZBvctSTevYiANR1u00POHjZBSdy5MxE1HTAjxq8nZBYTmylPwZDZD') {
     res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');    
   }
-  res.send('Error, wrong validation token');
-})
+});
