076d471cff7a623a486233acccc20ccc86ec4e6c
alexbulakh1994 <alexbulakh707@gmail.com>
2016-05-05 20:47:47 +0300

    Receive Message

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -22,9 +25,25 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+// To verify
 app.get('/webhook', function (req, res) {
   if (req.query['hub.verify_token'] === 'super_secret_code') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
+});
+
+//
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+      console.log(text);
+    }
+  }
+  res.sendStatus(200);
 });
