9edd25bdc22b5c93df1ac4e2fd2f8fa56a90e022
alexbulakh1994 <alexbulakh707@gmail.com>
2016-05-10 21:41:21 +0300

    send  people messages

--- index.js
+++ index.js
@@ -115,9 +115,10 @@ app.post('/webhook/', function (req, res) {
       	sendTextMessage(senderId, "Text received, echo: "+ text.substring(0, 200));
       	sendStructuredMessage(senderId);
     //})
-    }else if(req.body.entry[0].messaging[0].message.payload === 'send_test'){
-    	console.log("Connect structured messages!");
     }
+    // else if(req.body.entry[0].messaging[0].message.payload === 'send_test'){
+    // 	console.log("Connect structured messages!");
+    // }
   }
 
      
