12eabfdb2ba44a2bec53ea2879230f9ae2b2a369
alexbulakh1994 <alexbulakh707@gmail.com>
2016-05-11 19:04:27 +0300

    send  people messages

--- index.js
+++ index.js
@@ -131,14 +131,14 @@ app.post('/webhook/', function (req, res) {
     else {
     	if(event.postback && event.postback.payload === 'frontEnd_dev'){
     		sendTextMessage(senderId, "Hi frontEnd developer");
-    		structuredMessage.sendSpecializationMessage(request, token, senderId, structuredMessage.FrontEndPayload);
+    		structuredMessage.sendSpecializationMessage(requ, token, senderId, structuredMessage.FrontEndPayload);
     	}
     	if(event.postback && event.postback.payload === 'science'){
     		sendTextMessage(senderId, "Hi Science Reseacher");
     	}
     	if(event.postback && event.postback.payload === 'backEnd_dev'){
     		sendTextMessage(senderId, "Hi backEnd_dev");
-    		structuredMessage.sendSpecializationMessage(request, token, senderId, structuredMessage.backEndPayload);
+    		structuredMessage.sendSpecializationMessage(req, token, senderId, structuredMessage.backEndPayload);
     	}
     	}
 
