9ad1223441b1dd96963e409ce6b7494eb9f1c6ae
alexbulakh1994 <alexbulakh707@gmail.com>
2016-05-13 21:59:27 +0300

    send struct message

--- structed-messages.js
+++ structed-messages.js
@@ -1,6 +1,6 @@
-module.exports = {
 
-    chooose_spec: {
+var request = function(objArray){
+    return {
         attachment: {
             type: "template",
             payload: {
@@ -8,92 +8,95 @@ module.exports = {
                 text: "What do you want to do next?",
                 buttons: [{
                     type: "postback",
-                    title: "Back-End Developer",
-                    payload: "backEnd_dev"
+                    title: objArray[0].tittle,//"Back-End Developer",
+                    payload: objArray[0].payload//"backEnd_dev"
                 },
                 {
                     type: "postback",
-                    title: "Science Reseacher",
-                    payload: "science"
+                    title: objArray[1].tittle,//"Science Reseacher",
+                    payload: objArray[0].payload//"science"
                 },
                 {
                     type: "postback",
-                    title: "Front-End Developer",
-                    payload: "frontEnd_dev"
-                }]
-            }
-        }
-  },
-
-     messageDataBack : {
-        attachment: {
-            type: "template",
-            payload: {
-                template_type: "button",
-                text: "Choose specialization",
-                buttons: [{
-                    type: "postback",
-                    title: "Ruby",
-                    payload: "ruby_dev"
-                },
-                {
-                    type: "postback",
-                    title: "python",
-                    payload: "python_dev"
-                },
-                {
-                    type: "postback",
-                    title: "nodeJS",
-                    payload: "node_dev"
-                }]
-            }
-        }
-  },
-
- messageFrontDataBack : {
-        attachment: {
-            type: "template",
-            payload: {
-                template_type: "button",
-                text: "Choose specialization",
-                buttons: [{
-                    type: "postback",
-                    title: "Html, css",
-                    payload: "html_dev"
-                },
-                {
-                    type: "postback",
-                    title: "JavaScipt",
-                    payload: "javaScript_dev"
-                },
-                {
-                    type: "postback",
-                    title: "AngularJS",
-                    payload: "angular"
-                }]
-            }
-        }
-  },
-
- messageScienceResearch : {
-        attachment: {
-            type: "template",
-            payload: {
-                template_type: "button",
-                text: "Choose specialization",
-                buttons: [{
-                    type: "postback",
-                    title: "Python Network",
-                    payload: "python_net"
-                },
-                {
-                    type: "postback",
-                    title: "Apache Spark",
-                    payload: "apache"
+                    title: objArray[2].tittle,//"Front-End Developer",
+                    payload: objArray[0].payload//"frontEnd_dev"
                 }]
             }
         }
   }
+}
+module.exports = request;
 
-};
+
+//      messageDataBack : {
+//         attachment: {
+//             type: "template",
+//             payload: {
+//                 template_type: "button",
+//                 text: "Choose specialization",
+//                 buttons: [{
+//                     type: "postback",
+//                     title: "Ruby",
+//                     payload: "ruby_dev"
+//                 },
+//                 {
+//                     type: "postback",
+//                     title: "python",
+//                     payload: "python_dev"
+//                 },
+//                 {
+//                     type: "postback",
+//                     title: "nodeJS",
+//                     payload: "node_dev"
+//                 }]
+//             }
+//         }
+//   },
+
+//  messageFrontDataBack : {
+//         attachment: {
+//             type: "template",
+//             payload: {
+//                 template_type: "button",
+//                 text: "Choose specialization",
+//                 buttons: [{
+//                     type: "postback",
+//                     title: "Html, css",
+//                     payload: "html_dev"
+//                 },
+//                 {
+//                     type: "postback",
+//                     title: "JavaScipt",
+//                     payload: "javaScript_dev"
+//                 },
+//                 {
+//                     type: "postback",
+//                     title: "AngularJS",
+//                     payload: "angular"
+//                 }]
+//             }
+//         }
+//   },
+
+//  messageScienceResearch : {
+//         attachment: {
+//             type: "template",
+//             payload: {
+//                 template_type: "button",
+//                 text: "Choose specialization",
+//                 buttons: [{
+//                     type: "postback",
+//                     title: "Python Network",
+//                     payload: "python_net"
+//                 },
+//                 {
+//                     type: "postback",
+//                     title: "Apache Spark",
+//                     payload: "apache"
+//                 }]
+//             }
+//         }
+//   }
+
+// };
 
