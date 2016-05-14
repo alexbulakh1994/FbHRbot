b9b2ffa9ba7933f9de51d60ac093bd3d92cc2a15
alexbulakh1994 <alexbulakh707@gmail.com>
2016-05-14 16:26:09 +0300

    send struct message

--- postbacks.js
+++ postbacks.js
@@ -1,20 +1,20 @@
-var specialization = [{title: "Backend Developer", payload: "backEnd_dev"}, 
-    	 												  {title: "Science Reseacher", payload: "science"}, 
-    	 												  {title: "FrontEnd Developer", payload: "frontEnd_dev"}];
+var specialization = [{type: "postback", title: "Backend Developer", payload: "backEnd_dev"}, 
+    	 												  {type: "postback", title: "Science Reseacher", payload: "science"}, 
+    	 												  {type: "postback", title: "FrontEnd Developer", payload: "frontEnd_dev"}];
 
- var backEndPostbacks = [{title: "Ruby", payload: "ruby_dev"}, 
-    	 												  {title: "Python", payload: "python_dev"}, 
-    	 												  {title: "Node JS", payload: "node_dev"},
-    	 												  {title: "All skills was choosen", payload: "finish"}];
+ var backEndPostbacks = [{type: "postback", title: "Ruby", payload: "ruby_dev"}, 
+    	 												  {type: "postback", title: "Python", payload: "python_dev"}, 
+    	 												  {type: "postback", title: "Node JS", payload: "node_dev"},
+    	 												  {type: "postback", title: "All skills was choosen", payload: "finish"}];
 
- var scienceReseachPostbacks = [{title: "Python Network", payload: "python_net"}, 
-    	 												  {title: "Apache Spark", payload: "apache"},
-    	 												  {title: "All skills was choosen", payload: "finish"}];
+ var scienceReseachPostbacks = [{type: "postback", title: "Python Network", payload: "python_net"}, 
+    	 												  {type: "postback", title: "Apache Spark", payload: "apache"},
+    	 												  {type: "postback", title: "All skills was choosen", payload: "finish"}];
 
-var frontEndPostbacks = [{title: "HTML, CSS", payload: "html_dev"}, 
-    	 												  {title: "JavaScript", payload: "javaScript_dev"}, 
-    	 												  {title: "Angular JS", payload: "angular"},
-    	 												  {title: "All skills was choosen", payload: "finish"}];
+var frontEndPostbacks = [{type: "postback", title: "HTML, CSS", payload: "html_dev"}, 
+    	 												  {type: "postback", title: "JavaScript", payload: "javaScript_dev"}, 
+    	 												  {type: "postback", title: "Angular JS", payload: "angular"},
+    	 												  {type: "postback", title: "All skills was choosen", payload: "finish"}];
 
  module.exports.frontEnd = frontEndPostbacks;   
  module.exports.backEnd = backEndPostbacks;
--- structed-messages.js
+++ structed-messages.js
@@ -6,21 +6,7 @@ var request = function(objArray){
             payload: {
                 template_type: "button",
                 text: "What do you want to do next?",
-                buttons: [{
-                    type: "postback",
-                    title: objArray[0].title,//"Back-End Developer",
-                    payload: objArray[0].payload//"backEnd_dev"
-                },
-                {
-                    type: "postback",
-                    title: objArray[1].title,//"Science Reseacher",
-                    payload: objArray[1].payload//"science"
-                },
-                {
-                    type: "postback",
-                    title: objArray[2].title,//"Front-End Developer",
-                    payload: objArray[2].payload//"frontEnd_dev"
-                }]
+                buttons: objArray
             }
         }
   }
