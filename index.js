a91603f105c2133367d82b42a84a8b41b6694331
alexbulakh1994 <alexbulakh707@gmail.com>
2016-05-14 18:09:21 +0300

    send struct message

--- index.js
+++ index.js
@@ -109,8 +109,8 @@ app.post('/webhook/', function (req, res) {
     }else if(allSenders[senderId].states === 2 && technick_payloads.indexOf(event.postback.payload)!== -1){
     	console.log(postbacks.science.length);
     	if(postbacks.frontEnd.length === 2 || postbacks.backEnd.length === 2 || postbacks.science.length === 2){
+    			allSenders[senderId].states++;
     			sendMessage(senderId, {text:"What is last place of your work"});
-    			allSenders[senderId].states = 3;
     	}else{
     	switch(event.postback.payload){
     		case 'python_dev': 
