0addc471af0e62a4f1fbe235461072029cfa33d4
alexbulakh1994 <alexbulakh707@gmail.com>
2016-05-15 23:20:22 +0300

    send struct message

--- index.js
+++ index.js
@@ -163,8 +163,8 @@ app.post('/webhook/', function (req, res) {
     	}	
   }else if(allSenders[senderId].states === 4){
   		console.log('Object 2 is:');
-  		console.log(util.inspect(req.body.entry[0].messaging[i].message.attachment.type, false, null));
-  		console.log(util.inspect(req.body.entry[0].messaging[i].message.attachment.payload.url, false, null));
+  		console.log(util.inspect(req.body false, null));
+  		//console.log(util.inspect(req.body.entry[0].messaging[i].message.attachment.payload.url, false, null));
   		//sendMessage(senderId, {text:"Upload CV in doc or pdf format"});
   } 
 }
