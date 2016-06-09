c7692698e45a15871513816263ee1ded65c5e9ef
alexbulakh1994 <alexbulakh707@gmail.com>
2016-06-09 22:43:42 +0300

    'finish'

--- structed-messages.js
+++ structed-messages.js
@@ -14,50 +14,38 @@ Number.prototype.mod = function(n) {
 }
 
 var request = function(objArray, text){
-    var buttons = [];
-    var messagePostBack = [];
-    // if(currentListPosition !== undefined && objArray.length > MAX_BUTTON_NUMBERS){
-    //     if(currentListPosition < 0){
-    //         buttons.push(objArray[Number(currentListPosition).mod(objArray.length)]);
-    //     }else{
-    //          buttons.push(objArray[currentListPosition % objArray.length]);
-    //     }
-    //     buttons = buttons.concat(postbacks.previousNextButton);
-    // }else
-    //     buttons = objArray;
 
+    var messagePostBack = [];
     for(var i = 0; i < objArray.length; i++){
-        
-        if(i === objArray.length - 1){
-            messagePostBack.push( {
-                attachment: {
-                    type: "template",
-                    payload: {
-                        template_type: "button",
-                        text: text, 
-                        buttons: buttonsConstructor(objArray.slice(i - (objArray.length % 3) + 1))
-                    }
-                }
-            });
-           
-        }else if((i+1) % MAX_BUTTON_NUMBERS === 0){
-            messagePostBack.push( {
-                attachment: {
-                    type: "template",
-                    payload: {
-                        template_type: "button",
-                        text: 'Continue', 
-                        buttons: buttonsConstructor(objArray.slice(i - 2, i + 1))
-                    }
-                }
-            });
-  
+        if(objArray.length <= MAX_BUTTON_NUMBERS){
+            messagePostBack.push(payloadObj(text, objArray));
+            return messagePostBack;
+        }
+
+        if((i+1) % MAX_BUTTON_NUMBERS === 0){
+            messagePostBack.push(payloadObj('Continue', buttonsConstructor(objArray.slice(i - 2, i + 1))) );  
+        }else if(i === objArray.length - 1){
+            messagePostBack.push(payloadObj(text, buttonsConstructor(objArray.slice(i - (objArray.length % 3) + 1))) );
         }
 
     }
           
     return messagePostBack;
 }
+
+var payloadObj = function(text, buttons){
+    return{
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "button",
+                    text: text, 
+                    buttons: buttons
+                }
+            }
+        };
+}
+
 module.exports = request;
 
 
