c372e851057cefc7d5edf9c9d6120e602540d4f9
alexbulakh1994 <alexbulakh707@gmail.com>
2016-06-15 01:49:14 +0300

    finish

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ app.post('/webhook/', function (req, res) {
 
 function greeting(senderId){
 	 allSenders[senderId] = new client({states: 1});
-	 setTimeout(sendMessage(senderId, [{text: 'Hey. I HR-bot of the company “Dataroot”. If you want to work with us, then answer a few questions, and I will gather all the necessary information and will send it to our HR-manager.'}]), 500);
+	 setTimeout(sendMessage(senderId, [{text: 'Hey. I HR-bot of the company “Dataroot”. If you want to work with us, then answer a few questions, and I will gather all the necessary information and will send it to our HR-manager.'}]), 1000);
 	 setTimeout(sendMessage(senderId, [{text: 'To restart the chat - type the command \\ restart.'}]), 200);
 	 setTimeout(sendMessage(senderId, [{text: 'So begin. What is your full name?'}]), 200);
 	 
